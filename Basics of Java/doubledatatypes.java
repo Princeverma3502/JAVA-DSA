924d3ee89ceaf773eb63bee175d998e11dd3541b
Princeverma3502 <164482663+Princeverma3502@users.noreply.github.com>
2024-04-08 18:15:06 +0530

    Replacement of files

---
+++ Basics of Java/doubledatatypes.java
@@ -0,0 +1,15 @@
+package BasicsOfJava;
+
+public class doubledatatypes {
+    public static void main(String[]args){
+        double x=5;
+        double y=2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        double z=5/2;
+        System.out.println(z);
+
+    }
+}
